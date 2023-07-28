758263429baf00fdcc3f74c031ae5f1fd866be53
Kane <kane.ly@digibank.com>
2023-07-28 14:42:10 +0700

    update new code

--- README.md
+++
@@ -1,6 +0,0 @@
-0000: Thành công
-0001: Username đã tồn tại
-0002: Đăng ký/Đăng nhập thiếu username/password
-0003: Đăng nhập với tài khoản không tồn tại
-0004: Đăng nhập sai mật khẩu
-0005: Xử lý không thành công quá 5 lần
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.philip01;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Philip01Application {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Philip01Application.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
